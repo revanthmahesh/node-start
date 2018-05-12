33b0acecc3e2eb64ba439a8c313e0cbd76ae0aa4
revanth mahesh <exclusiverevanth@gmail.com>
2018-05-12 15:26:44 +0530

    fs commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join(__dirname,'README.md'),function (error,data) {
+    if(error){return console.error(error)}
+    console.log(data);
+})
+
+fs.writeFile(__dirname+'/assets/magic.txt','magic happen in node!!',function(error, data){
+  if(error){return console.error(error)};
+  console.log(data)
+})
